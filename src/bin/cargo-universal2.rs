3d8192c83455303bf9894055d5d8485245e32102
valkyrie_pilot <valk@randomairborne.dev>
2024-01-30 19:35:40 -0700

    More consistent imports

--- src/bin/cargo-universal2.rs
+++ src/bin/cargo-universal2.rs
@@ -1,9 +1,8 @@
 use console::style;
-use std::process;
 
 fn main() {
     if let Err(error) = universal2::run() {
         println!("{} {}", style("error:").bold().red(), error);
-        process::exit(1);
+        std::process::exit(1);
     }
 }
